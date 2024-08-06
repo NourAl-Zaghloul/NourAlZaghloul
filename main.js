ea397e9420c35c2bf85ae222a3246b6c4783aafc
nour al-Zaghloul <nouralzaghloul@gmail.com>
2024-08-06 10:54:16 -0400

    Slightly more general version

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function setActive(location){
         case "About":
             document.getElementById("tabs").style.gridTemplateColumns = "1.5fr 1fr 1fr";  
 
-            document.getElementById("CVResume").classList.remove("active");
+            document.getElementById("Resume").classList.remove("active");
             document.getElementById("buttonResume").classList.remove("active");
             document.getElementById("ContactMe").classList.remove("active");
             document.getElementById("buttonContact").classList.remove("active");
@@ -21,13 +21,13 @@ function setActive(location){
             document.getElementById("buttonAbout").classList.remove("active");
 
 
-            document.getElementById("CVResume").classList.add("active");
+            document.getElementById("Resume").classList.add("active");
             document.getElementById("buttonResume").classList.add("active");
             break;
         case "Contact":
             document.getElementById("tabs").style.gridTemplateColumns = "1fr 1fr 1.5fr";  
 
-            document.getElementById("CVResume").classList.remove("active");
+            document.getElementById("Resume").classList.remove("active");
             document.getElementById("buttonResume").classList.remove("active");
             document.getElementById("AboutNour").classList.remove("active");
             document.getElementById("buttonAbout").classList.remove("active");
